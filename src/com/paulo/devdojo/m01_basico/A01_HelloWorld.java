36a2c080f96c895b18e96acf4916290a8e47f731
Paulo Nogueira <paulobh.nogueira@gmail.com>
2023-01-02 11:34:58 -0300

    Criação do repositório do curso.

---
+++ src/com/paulo/devdojo/m01_basico/A01_HelloWorld.java
@@ -0,0 +1,7 @@
+package com.paulo.devdojo.m01_basico;
+
+public class A01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/paulo/devdojo/m01_basico/A10_TiposPrimitivos.java
@@ -0,0 +1,7 @@
+package com.paulo.devdojo.m01_basico;
+
+public class A10_TiposPrimitivos {
+    public static void main(String[] args) {
+
+    }
+}
