6ac9bf99c76725e1289a300519d2942858fa14d1
Paulo Nogueira <paulobh.nogueira@gmail.com>
2023-02-02 16:47:10 -0300

    Métodos úteis com Strings.

--- src/com/paulo/devdojo/utilityClasses/c02_strings/A108_Strings.java
+++ src/com/paulo/devdojo/utilityClasses/c02_strings/A108_Strings.java
@@ -1,2 +1,44 @@
-package com.paulo.devdojo.utilityClasses.c02_strings;public class A108_Strings {
+package com.paulo.devdojo.utilityClasses.c02_strings;
+
+/* Strings são utilizadas para armazenamento de textos.
+   Elas são imutáveis e tem uma área reservada apenas para Strings, chamada "pool de strings".
+
+   Quando temos duas variáveis String com o mesmo valor, o Java não cria dois espaços em memória. Ele utiliza a mesma referência
+   para a String que já está criada no pool de Strings.
+
+   Quando criamos uma String utilizando a sintaxe de criação de objetos,
+ */
+public class A108_Strings {
+    public static void main(String[] args) {
+        String name = "Paulo";
+        String surname = "Nogueira";
+
+        System.out.println(name + " " + surname);
+
+        String paulo = "Paulo";
+
+        /* É impossível alterar uma String. É necessário criar uma nova variável de referência. No caso abaixo, temos três variáveis:
+           Nome, sobrenome e fullName, que é a concatenação do nome e Sobrenome. */
+        String fullName = name + " " + surname;
+        System.out.println(fullName);
+
+        /* Quando comparamos Srings devemos utilizar o equals(), pois ele é utilizado para comparações de objetos e garante melhor performance.
+           Neste caso, o valor da String será utilizado na comparação. */
+        System.out.println(name.equals(paulo));
+        System.out.println(name == paulo);
+        System.out.println(name == surname);
+        System.out.println("---------------------------------------");
+
+        /* Porém, quando criamos Strings com sintaxe de objetos, a referência passa a ser do Objeto String e não do valor em si.
+           Neste caso, devemos comparar uma String lembrando do conceito de Intern. Ou seja, deve-se comparar o valor contido na
+           String, e não o seu espaço em memória.
+
+           Um outro detalhe importante é que Será criado um objeto String no HEAP e uma String no POOL de Strings. */
+
+        String objectString = new String("Paulo");
+        String literalString = "Paulo";
+
+        System.out.println(objectString == literalString);
+        System.out.println(objectString.intern().equals(literalString));
+    }
 }
---
+++ src/com/paulo/devdojo/utilityClasses/c02_strings/A109_UtilityMethods.java
@@ -0,0 +1,36 @@
+package com.paulo.devdojo.utilityClasses.c02_strings;
+
+/* Strings fornecem uma quantidade de métodos úteis, que podem ser utilizados para manipulação do valor da String. */
+public class A109_UtilityMethods {
+    public static void main(String[] args) {
+        String fullName = "Paulo Nogueira";
+        String firstName = fullName.substring(0, fullName.indexOf(" "));
+        String lastName = fullName.substring(fullName.lastIndexOf(" ") + 1); // Última posição excluída.
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+        System.out.println("---------------------------------------------");
+
+        for(int i = 0; i < fullName.length(); i++) {
+            System.out.print(fullName.charAt(i) + " ");
+        }
+        System.out.println("\n---------------------------------------------");
+
+        String job = "I will be a new fullstack developer.";
+        job = job.replace("fullstack", "java");
+        System.out.println(job);
+
+        String jobFull = "I will be a new big bixo in fullStack developer";
+        jobFull = jobFull
+                .replace("will be", "am")
+                .replace("fullstack", "java")
+                .replace("new", "very")
+                .replace("bixo", "bicho");
+        System.out.println(jobFull);
+        System.out.println("--------------------------------------------");
+
+        System.out.println(job.toLowerCase());
+        System.out.println(jobFull.toUpperCase());
+    }
+}
